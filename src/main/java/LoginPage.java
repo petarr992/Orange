0fda13c69a64d3f5caec64c84eb55468d70ff0f4
petarr992 <radosavljevicpetar92@gmail.com>
2023-03-30 20:54:13 +0200

    MyInfoTest

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends BasePage {
+@FindBy(name = "username")
+    WebElement userName;
+@FindBy(name = "password")
+    WebElement password;
+@FindBy(css = ".orangehrm-login-button")
+    WebElement loginButton;
+
+@FindBy(css = ".oxd-alert-content-text")
+WebElement errorMessage;
+
+
+public LoginPage(ChromeDriver driver){
+    this.driver = driver;
+    PageFactory.initElements(driver,this);
+
+   }
+   public String error(){
+    return errorMessage.getText();
+
+   }
+   public void Login(String name,String pass){
+    userName.sendKeys(name);
+    password.sendKeys(pass);
+    loginButton.submit();
+
+   }
 }
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class BaseTest {
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+    public ChromeDriver openBrowser(){
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
+        return driver;
+
+    }
 }
